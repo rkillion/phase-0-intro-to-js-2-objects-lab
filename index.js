2341a22c79c4dc33ab01ff1092d80eb071ab9786
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-06-10 20:24:38 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+const employee = {name: "name",streetAddress: "street address"};
+
+let updateEmployeeWithKeyAndValue = (employee,key,value) => Object.assign({},employee,{[key]:value});
+let destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => Object.assign(employee,{[key]:value});
+function deleteFromEmployeeByKey (employee, key) {
+    let employeeClone = Object.assign({},employee);
+    delete employeeClone[key];
+    return employeeClone;
+}
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
